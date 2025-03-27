666fca03f8b66a2e9234741ac6d72f006578f934
NHP0820 <lozy-wm21@student.tac.edu.my>
2025-03-27 16:25:25 +0800

    show all changes

--- app/page/cancel_order.php
+++ app/page/cancel_order.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $reason = $_POST['reason'];
     try {
         $cancelOrder = $_db->prepare("UPDATE `order` SET status_id = 4 , cancel_desc = ? WHERE order_id = ?");
-        $cancelOrder->execute([$reason,$orderId]);
+        $cancelOrder->execute([$reason, $orderId]);
 
         if ($cancelOrder->rowCount() > 0) {
             echo json_encode(['success' => true]);
